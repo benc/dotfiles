c3f3268faf7f33cc6711c2d2fa9d97c9fe9a35de
Ben Cochez <ben@cochez.info>
2015-06-04 19:39:25 +0200

    pass on args to VS Code
    still can't create new files though, touch them first as a workaround

--- fish_custom/plugins/code/code.fish
+++ fish_custom/plugins/code/code.fish
@@ -1,6 +1,6 @@
 function code --description 'Open Visual Studio Code'
   if test -d "/Applications/Visual Studio Code.app"
-    open -a "/Applications/Visual Studio Code.app" $argv
+    open -a "/Applications/Visual Studio Code.app" --new --args $argv
   else
     echo "No Visual Studio Code installation found"
   end
