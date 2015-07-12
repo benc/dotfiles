6be499dc74a08fa3697b5b111e948d25894c840b
Ben Cochez <ben@cochez.info>
2015-07-12 07:32:33 +0200

    code v0.5.0 uses electron

--- fish_custom/plugins/code/code.fish
+++ fish_custom/plugins/code/code.fish
@@ -1,6 +1,6 @@
 function code --description 'Open Visual Studio Code'
   if test -d "/Applications/Visual Studio Code.app"
-    "/Applications/Visual Studio Code.app/Contents/MacOS/Atom" $argv
+    "/Applications/Visual Studio Code.app/Contents/MacOS/Electron" $argv
   else
     echo "No Visual Studio Code installation found"
   end
