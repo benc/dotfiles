853ea76a059686bcdd913a0a43f92d51d9a7c1fd
Ben Cochez <ben@cochez.info>
2015-12-15 19:31:55 +0100

    postgres 9.5

--- config/omf/init.fish
+++ config/omf/init.fish
@@ -1,7 +1,7 @@
 set -x MAVEN_OPTS "-Xmx512m"
 set -x JAVA_OPTS "-Djava.awt.headless=true -Dfile.encoding=UTF-8"
 set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
-set -x PATH /Applications/Postgres.app/Contents/Versions/9.4/bin $PATH
+set -x PATH /Applications/Postgres.app/Contents/Versions/9.5/bin $PATH
 set -x PATH ./bin ./bin/build_pipeline ./node_modules/.bin $PATH
 set -x VAGRANT_DEFAULT_PROVIDER vmware_fusion
 ulimit -n 65536
