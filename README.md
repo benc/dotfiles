2f87f200b2c2d6c482f7af549cf4b0f067f10fc4
Ben Cochez <ben@cochez.consulting>
2023-05-13 12:09:17 +0200

    windows (wip)

--- README.md
+++ README.md
@@ -1,12 +1,38 @@
-# Install on macos/linux/wsl
+# Install
+
+## Macos/linux/wsl
 
     sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply benc
 
-(Windows: TODO)
+## Windows 11
+
+Prerequisites:
+
+    winget install --id Microsoft.Powershell --source winget
+    winget install --id Microsoft.WindowsTerminal
+
+Prerequisites, run in an *elevated* powershell prompt:
+
+    # gsudo
+    winget install --exact gerardog.gsudo --interactive
+
+    # git
+    winget install --exact Git.Git --interactive
+
+    # chocolatey
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+    # age
+    choco install age.portable
+
+Run in a *regular* powershell prompt:
+
+    (irm -useb https://get.chezmoi.io/ps1) | powershell -c -
+    .\bin\chezmoi.exe init --apply benc
 
 # Usage
 
-## Most used commans:
+## Most used commands:
 
     # edit dotfiles
     chezmoi edit
@@ -16,3 +42,8 @@
 
     # update system with the latest dotfiles
     chezmoi update
+
+## Troubleshooting
+
+    chezmoi -v apply
+    chezmoi doctor
---
+++ run_install-windows.ps1
@@ -0,0 +1,81 @@
+Write-Host "Request administrator access"
+gsudo {
+    Write-Host "Update choco packages and modules"
+    choco upgrade all -y
+    Update-Module
+
+    Write-Host "`nInstall choco packages"
+    choco install bat delta dust fd fzf lf ripgrep -y
+
+    Write-Host "`nInstall modules"
+    Install-Module git-aliases -Scope CurrentUser -Allowclobber
+    Install-Module -Name PSFzf
+    Install-Module posh-git -Scope CurrentUser -AllowClobber
+    Install-Module -Name Terminal-Icons -Repository PSGallery
+
+    Write-Host "`nSet execution policy"
+    Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+}
+
+# install hyperv
+# DISM /Online /Enable-Feature /All /FeatureName:Microsoft-Hyper-V /all
+
+# install wsl2
+# wsl --install
+# wsl --set-default-version 2
+
+# wsl --update
+# wsl --install -d Ubuntu
+
+# Stop ubuntu
+# wsl --stop -d Ubuntu
+#
+
+# Add .wslconfig script with:
+#
+# In elevated cmd, run
+# netsh interface portproxy delete v4tov4 listenaddress=0.0.0.0 listenport=2222
+# netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=2222 connectaddress=192.168.1.44 connectport=2222
+# # netsh interface portproxy show all
+# # netsh interface portproxy delete v4tov4 listenaddress=0.0.0.0 listenport=2222
+# netsh advfirewall firewall add rule name="Open Port 2222 for WSL2" dir=in action=allow protocol=TCP localport=2222
+
+# TODO setup SSH
+#
+# TODO add reg key to set shell to bash with a default wsl distro - so jetbrains remote works WITHOUT setting up SSH in WSL2!
+# wsl --install -d Ubuntu
+# wsl --set-default Ubuntu
+# New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\WINDOWS\System32\bash.exe" -PropertyType String -Force
+
+Write-Host "Update winget packages"
+winget upgrade --all
+Write-Host("")
+
+function wingetInstallIfNotInstalled($id) {
+    Write-Host("Install $id if not installed")
+    winget list --exact --id $id || winget install --exact --id $id --interactive
+    Write-Host("")
+}
+
+wingetInstallIfNotInstalled("AgileBits.1Password")
+wingetInstallIfNotInstalled("Starship.Starship")
+wingetInstallIfNotInstalled("ajeetdsouza.zoxide")
+wingetInstallIfNotInstalled("Eugeny.Tabby")
+wingetInstallIfNotInstalled("ScooterSoftware.BeyondCompare4")
+wingetInstallIfNotInstalled("Microsoft.Sysinternals.ProcessExplorer")
+wingetInstallIfNotInstalled("Microsoft.WindowsTerminal")
+wingetInstallIfNotInstalled("JetBrains.Toolbox")
+wingetInstallIfNotInstalled("SaaSGroup.Tower")
+wingetInstallIfNotInstalled("Vercel.Hyper")
+wingetInstallIfNotInstalled("WinSCP.WinSCP")
+wingetInstallIfNotInstalled("Giorgiotani.Peazip")
+wingetInstallIfNotInstalled("Microsoft.VisualStudioCode")
+wingetInstallIfNotInstalled("Nvidia.GeForceExperience")
+wingetInstallIfNotInstalled("Intel.IntelDriverAndSupportAssistant")
+wingetInstallIfNotInstalled("Docker.DockerDesktop")
+wingetInstallIfNotInstalled("Git.Git")
+wingetInstallIfNotInstalled("gerardog.gsudo")
+wingetInstallIfNotInstalled("tailscale.tailscale")
+wingetInstallIfNotInstalled("OpenJS.NodeJS.LTS")
+wingetInstallIfNotInstalled("Zoom.Zoom")
+wingetInstallIfNotInstalled("IVPN.IVPN")
