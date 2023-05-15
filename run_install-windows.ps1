eda48106d96c296dc641a5454fbd5bf84d0da04d
Ben Cochez <ben@cochez.consulting>
2023-05-15 18:09:09 +0200

    windows (wip)

--- run_install-windows.ps1
+++ run_install-windows.ps1
@@ -5,7 +5,7 @@ gsudo {
     Update-Module
 
     Write-Host "`nInstall choco packages"
-    choco install bat delta fd fzf lf ripgrep -y
+    choco install bat delta fd fzf lf ripgrep op npiperelay -y
 
     Write-Host "`nInstall modules"
     Install-Module git-aliases -Scope CurrentUser -Allowclobber
@@ -17,19 +17,12 @@ gsudo {
     Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 }
 
-# install hyperv
+# hyperv
 DISM /Online /Enable-Feature /All /FeatureName:Microsoft-Hyper-V /all
 
-# install wsl2
-# wsl --install
-# wsl --set-default-version 2
-
-# wsl --update
-# wsl --install -d Ubuntu
-
-# Stop ubuntu
-# wsl --stop -d Ubuntu
-#
+# wsl2
+wsl --update
+# wsl --install -d Ubuntu-22.04
 
 # Add .wslconfig script with:
 #
@@ -41,10 +34,6 @@ DISM /Online /Enable-Feature /All /FeatureName:Microsoft-Hyper-V /all
 # netsh advfirewall firewall add rule name="Open Port 2222 for WSL2" dir=in action=allow protocol=TCP localport=2222
 
 # TODO setup SSH
-#
-# TODO add reg key to set shell to bash with a default wsl distro - so jetbrains remote works WITHOUT setting up SSH in WSL2!
-# wsl --install -d Ubuntu
-# wsl --set-default Ubuntu
 # New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\WINDOWS\System32\bash.exe" -PropertyType String -Force
 
 Write-Host "Update winget packages"
@@ -73,7 +62,5 @@ wingetInstallIfNotInstalled("Docker.DockerDesktop")
 wingetInstallIfNotInstalled("Git.Git")
 wingetInstallIfNotInstalled("gerardog.gsudo")
 wingetInstallIfNotInstalled("tailscale.tailscale")
-wingetInstallIfNotInstalled("OpenJS.NodeJS.LTS")
-wingetInstallIfNotInstalled("Zoom.Zoom")
 wingetInstallIfNotInstalled("IVPN.IVPN")
 wingetInstallIfNotInstalled("Logitech.LogiTune")
