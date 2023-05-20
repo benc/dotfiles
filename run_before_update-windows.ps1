b32a6edaa467d3a8b1fd719829db8416e4335cb5
Ben Cochez <ben@kyber.be>
2023-05-20 22:41:35 +0200

    update portainer agent

--- run_before_update-windows.ps1
+++ run_before_update-windows.ps1
@@ -7,3 +7,9 @@ gsudo {
 Write-Host "Update winget packages"
 winget upgrade --all
 Write-Host("")
+
+Write-Host "Run the latest portainer agent"
+docker stop portainer_agent
+docker rm portainer_agent
+docker pull portainer/agent:latest
+docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent:latest
