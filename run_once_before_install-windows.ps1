ba347cf84b2ae3a17fcbe3d868aa77eca4e3246f
Ben Cochez <ben@kyber.be>
2023-05-22 13:29:16 +0200

    change default keyboard to us qwerty (nl-be language)
    Signed-off-by: Ben Cochez <ben@kyber.be>

--- run_once_before_install-windows.ps1
+++ run_once_before_install-windows.ps1
@@ -96,6 +96,17 @@ gsudo {
 
     # hyperv
     DISM /Online /Enable-Feature /All /FeatureName:Microsoft-Hyper-V /all
+
+    # set keyboard to en-us, with nl-be as language
+    $CurrentList = Get-WinUserLanguageList
+    $CurrentList.Clear()
+
+    $Lang = New-WinUserLanguageList nl-BE
+    $Lang[0].InputMethodTips.Clear()
+    $Lang[0].InputMethodTips.Add("0409:00000409") # US QWERTY layout
+    Set-WinUserLanguageList $Lang -Force
+
+    Get-WinUserLanguageList
 }
 
 refreshenv
