aafaf5c022daacf5e3aeab11f323ba06e7900e12
Ben Cochez <ben@kyber.be>
2023-05-24 19:53:26 +0200

    powershell updates

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,28 +1,31 @@
-#$executionTime = Measure-Command `
-#{
-Import-Module PSReadLine # before PSFzf
-Import-Module PsFzf
+# Starship
+if (Get-Command starship -errorAction SilentlyContinue) {
+    Invoke-Expression (&starship init powershell)
+}
 
-# $ProfilePath=Split-Path -parent $profile
+# fzf
+if (Get-Command fzf -errorAction SilentlyContinue) {
+    Import-Module PSReadLine # before PSFzf
+    Import-Module PsFzf
 
-# PSReadLine configuration
-# Shows navigable menu of all options when hitting Tab
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+    # PSReadLine configuration
+    # Shows navigable menu of all options when hitting Tab
+    Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
-# PsFzf configuration
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
-$env:FZF_DEFAULT_OPTS = '--height 40% --layout=reverse --info=inline'
-# https://github.com/junegunn/fzf/wiki/Windows#relative-filepaths
-$env:FZF_DEFAULT_COMMAND = 'rg --files . 2> nul'
+    # PsFzf configuration
+    Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+    $env:FZF_DEFAULT_OPTS = '--height 40% --layout=reverse --info=inline'
+    # https://github.com/junegunn/fzf/wiki/Windows#relative-filepaths
+    $env:FZF_DEFAULT_COMMAND = 'rg --files . 2> nul'
+}
 
 # Zoxide: https://github.com/ajeetdsouza/Zoxide
-Invoke-Expression (& {
-    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
-    (zoxide init --hook $hook powershell | Out-String)
-})
+if (Get-Command zoxide -errorAction SilentlyContinue) {
+    Invoke-Expression (& { (zoxide init powershell | Out-String) })
+}
 
-# Prompt
-Invoke-Expression (&starship init powershell)
-
-#}
-#Write-Host "$PSCommandPath execution time: $executionTime"
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
+}
