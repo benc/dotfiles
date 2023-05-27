a481fefe54a4afeff9a080209bd4ca148712d9ab
Ben Cochez <ben@kyber.be>
2023-05-27 14:22:43 +0200

    towards feature parity in powershell

--- dot_config/powershell/profile.d/30-aliases.ps1
+++ dot_config/powershell/profile.d/30-aliases.ps1
@@ -1,1 +1,14 @@
 New-Alias -Name codei -Value code-insiders
+
+function jkill ($Id) {
+    jps -l | Where-Object { $_ -match $Id } | ForEach-Object {
+        $pid = ($_ -split '\s+')[0]
+        Stop-Process -Id $pid -Force
+    }
+}
+
+function Nuke-Modules {
+    Remove-Item -Path "node_modules" -Recurse -Force
+    npm install
+    npm prune
+}
