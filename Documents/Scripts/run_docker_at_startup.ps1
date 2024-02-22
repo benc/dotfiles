751fefd17dee675b2235f3fabdaad3785025cfa3
Ben Cochez <benc>
2024-02-22 20:19:07 +0100

    run docker on startup + remove geforce experience (replaced by nvidia app)

---
+++ Documents/Scripts/run_docker_at_startup.ps1
@@ -0,0 +1,1 @@
+Start-Process "C:\Program Files\Docker\Docker\Docker Desktop.exe"
--- run_once_before_install-windows.ps1
+++ run_once_before_install-windows.ps1
@@ -13,7 +13,6 @@ wingetInstallIfNotInstalled("SaaSGroup.Tower")
 wingetInstallIfNotInstalled("WinSCP.WinSCP")
 wingetInstallIfNotInstalled("Giorgiotani.Peazip")
 wingetInstallIfNotInstalled("Microsoft.VisualStudioCode")
-wingetInstallIfNotInstalled("Nvidia.GeForceExperience")
 wingetInstallIfNotInstalled("Intel.IntelDriverAndSupportAssistant")
 wingetInstallIfNotInstalled("Docker.DockerDesktop")
 wingetInstallIfNotInstalled("ProxymanLLC.Proxyman")
@@ -78,9 +77,10 @@ gsudo {
     netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=$Port connectaddress=$wsl_ip connectport=2222
     netsh interface portproxy show v4tov4
 
+    $username = [Environment]::UserName
+
     # autostart wsl2 on boot
     $Action = New-ScheduledTaskAction -Execute "pwsh.exe" -Argument "-ExecutionPolicy Bypass -File `"$env:USERPROFILE\Documents\Scripts\run_wsl2_at_startup.ps1`""
-    $username = [Environment]::UserName
     $Trigger = New-ScheduledTaskTrigger -AtLogOn -User $username
     $Trigger.Delay = "PT15S" # 15 second delay
     $Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -DontStopOnIdleEnd
@@ -88,16 +88,29 @@ gsudo {
     $Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Principal $Principal -Settings $Settings
     Register-ScheduledTask "Start WSL2" -InputObject $Task -Force
 
-
     # proxy wsl2 ssh on boot
     $Action = New-ScheduledTaskAction -Execute 'pwsh.exe' -Argument "-ExecutionPolicy Bypass -File `"$env:USERPROFILE\Documents\Scripts\proxy_wsl2.ps1`""
-    $username = [Environment]::UserName
     $Trigger = New-ScheduledTaskTrigger -AtLogon -User $username
     $Trigger.Delay = "PT30S" # 30 second delay
     $Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -DontStopOnIdleEnd
     $Principal = New-ScheduledTaskPrincipal -UserId $username -LogonType Interactive -RunLevel Highest
     $Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Principal $Principal -Settings $Settings
     Register-ScheduledTask "Route WSL2 SSH" -InputObject $Task -Force
+
+    # autostart docker on boot
+    $Action = New-ScheduledTaskAction -Execute 'pwsh.exe' -Argument "-ExecutionPolicy Bypass -File `"$env:USERPROFILE\Documents\Scripts\run_docker_at_startup.ps1`""
+    $Trigger = New-ScheduledTaskTrigger -AtLogon -User $username
+    $Trigger.Delay = "PT10S" # 10 second delay
+    $Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -DontStopOnIdleEnd
+    $Principal = New-ScheduledTaskPrincipal -UserId $username -LogonType Interactive -RunLevel Highest
+    $Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Principal $Principal -Settings $Settings
+    Register-ScheduledTask "Start Docker Desktop" -InputObject $Task -Force
+
+    # add user to docker-users group
+    try {
+        Add-LocalGroupMember -Group docker-users -Member $username -ErrorAction Stop
+    } catch [Microsoft.PowerShell.Commands.MemberExistsException] {
+    }
 
     # hyperv
     DISM /Online /Enable-Feature /All /FeatureName:Microsoft-Hyper-V /all
