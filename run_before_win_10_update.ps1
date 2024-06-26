6d518827f6f90dc0d7a6c739930f62fb09baae49
Ben Cochez <ben@kyber.be>
2024-06-26 12:57:04 +0200

    win improvements

--- run_before_win_10_update.ps1
+++ run_before_win_10_update.ps1
@@ -15,3 +15,6 @@ docker stop portainer_agent
 docker rm portainer_agent
 docker pull portainer/agent:latest
 docker run -d -p 9001:9001 --name portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker/volumes:/var/lib/docker/volumes portainer/agent:latest
+
+Write-Host "Log into 1password"
+Invoke-Expression $(op signin)
