774f80b6946656f9ac11cf2d663b13fa8b0659ef
Ben Cochez <ben@kyber.be>
2024-06-28 13:23:12 +0200

    revert speedup winget

--- run_after_win_10_winget.ps1
+++ run_after_win_10_winget.ps1
@@ -1,6 +1,6 @@
-Write-Host "Update winget packages"
-winget import -i .winget.json --accept-source-agreements --accept-package-agreements --disable-interactivity
-winget upgrade --all --accept-source-agreements --accept-package-agreements --silent --include-unknown
+# Write-Host "Update winget packages"
+# winget import -i .winget.json --accept-source-agreements --accept-package-agreements --disable-interactivity
+# winget upgrade --all --accept-source-agreements --accept-package-agreements --silent --include-unknown
 
-# miniconda cannot be updated with winget, needs to be done with conda itself
-conda update -c base conda -y
+# # miniconda cannot be updated with winget, needs to be done with conda itself
+# conda update -c base conda -y
