7f8dcc443a11e1970cf1e876482164260115fe9d
Ben Cochez <ben@kyber.be>
2024-06-28 15:20:25 +0200

    fix psfzf

--- dot_config/powershell/profile.d/11-commands.ps1
+++ dot_config/powershell/profile.d/11-commands.ps1
@@ -26,17 +26,12 @@ if (Get-Command fzf -errorAction SilentlyContinue) {
     # If not loaded, import the module
     Import-Module PSReadLine
   }
-  Import-Module PsFzf
+ 
+  Set-PSReadLineOption -EditMode Emacs
+  Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 
-  # PSReadLine configuration
-  # Shows navigable menu of all options when hitting Tab
-  Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-
-  # PsFzf configuration
-  Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
-  $env:FZF_DEFAULT_OPTS = '--height 40% --layout=reverse --info=inline'
-  # https://github.com/junegunn/fzf/wiki/Windows#relative-filepaths
-  $env:FZF_DEFAULT_COMMAND = 'rg --files . 2> nul'
+  Import-Module PSFzf
+  Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+o' -PSReadlineChordReverseHistory 'Ctrl+r'
 }
 
 # Chocolatey
