74a4510de13d1aa54bb8ec59b52aa6808b0161ed
Ben Cochez <benc>
2024-06-29 21:04:01 +0200

    fix direnv

--- dot_config/powershell/profile.d/11-commands.ps1
+++ dot_config/powershell/profile.d/11-commands.ps1
@@ -6,8 +6,6 @@ if (Get-Command direnv -errorAction SilentlyContinue) {
   Invoke-Expression "$(direnv hook pwsh)"
   
   $env:DIRENV_CONFIG = $env:APPDATA + '\direnv\config'
-  $env:XDG_CACHE_HOME = $env:APPDATA + '\direnv\cache'
-  $env:XDG_DATA_HOME = $env:APPDATA + '\direnv\data'
 }
 
 # Zoxide
