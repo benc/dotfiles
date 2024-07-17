b6b0672673efdbdd0282e1242a575f5efe9d57ff
Ben Cochez <ben@kyber.be>
2024-07-17 13:44:07 +0200

    comfyui fix update, whitespace

--- scripts/comfyui/install-update-macos.sh
+++ scripts/comfyui/install-update-macos.sh
@@ -11,8 +11,8 @@ set -e
 
 ARCH=$(uname -m)
 if [ "$ARCH" != "arm64" ]; then
-    echo "This script is intended for ARM Macs only, skipping..."
-    exit 0
+  echo "This script is intended for ARM Macs only, skipping..."
+  exit 0
 fi
 
 COMFYUI_DIR=$HOME/Applications/ComfyUI
@@ -22,43 +22,43 @@ MODEL_DIR=$HOME/Models
 
 # comfy install is broken... https://github.com/Comfy-Org/comfy-cli/issues/98
 if [ ! -d "$COMFYUI_DIR" ]; then # install
-    echo "💡 Cloning comfyui..." 
-    git clone https://github.com/comfyanonymous/ComfyUI.git "$COMFYUI_DIR"
-    pushd "$COMFYUI_DIR/custom_nodes" || exit
-    git clone https://github.com/ltdrdata/ComfyUI-Manager.git
-    popd
+  echo "💡 Cloning comfyui..."
+  git clone https://github.com/comfyanonymous/ComfyUI.git "$COMFYUI_DIR"
+  pushd "$COMFYUI_DIR/custom_nodes" || exit
+  git clone https://github.com/ltdrdata/ComfyUI-Manager.git
+  popd
 
-    pushd "$COMFYUI_DIR" || exit
-    # uv venv --python "$(conda info --base)/bin/python3" && source .venv/bin/activate
-    uv venv && source .venv/bin/activate
+  pushd "$COMFYUI_DIR" || exit
+  # uv venv --python "$(conda info --base)/bin/python3" && source .venv/bin/activate
+  uv venv && source .venv/bin/activate
 
-    echo "layout python3" >> .envrc
-    echo "source .venv/bin/activate" >> .envrc
-    direnv allow .
+  echo "layout python3" >>.envrc
+  echo "source .venv/bin/activate" >>.envrc
+  direnv allow .
 
-    echo "💡 Installing comfyui dependencies"
-    uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cpu
-    uv pip install comfy-cli
-    uv pip install --upgrade certifi pip
-    # uv pip -y install onnxruntime-silicon
-    uv pip install -r requirements.txt
-    popd
+  echo "💡 Installing comfyui dependencies"
+  uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cpu
+  uv pip install comfy-cli
+  uv pip install --upgrade certifi pip
+  # uv pip -y install onnxruntime-silicon
+  uv pip install -r requirements.txt
+  popd
 
-    comfy set-default "$COMFYUI_DIR"
+  comfy set-default "$COMFYUI_DIR"
 
-    echo "💡 Setting up model dirs"
-    mkdir -p "$MODEL_DIR/StableDiffusion"
-    mkdir -p "$MODEL_DIR/Clip"
-    mkdir -p "$MODEL_DIR/ClipVision"
-    mkdir -p "$MODEL_DIR/Configs"
-    mkdir -p "$MODEL_DIR/ControlNet"
-    mkdir -p "$MODEL_DIR/Embeddings"
-    mkdir -p "$MODEL_DIR/Loras"
-    mkdir -p "$MODEL_DIR/Upscale"
-    mkdir -p "$MODEL_DIR/VAE"
-        
-    pushd "$COMFYUI_DIR" || exit
-    cat > extra_model_paths.yaml <<EOL
+  echo "💡 Setting up model dirs"
+  mkdir -p "$MODEL_DIR/StableDiffusion"
+  mkdir -p "$MODEL_DIR/Clip"
+  mkdir -p "$MODEL_DIR/ClipVision"
+  mkdir -p "$MODEL_DIR/Configs"
+  mkdir -p "$MODEL_DIR/ControlNet"
+  mkdir -p "$MODEL_DIR/Embeddings"
+  mkdir -p "$MODEL_DIR/Loras"
+  mkdir -p "$MODEL_DIR/Upscale"
+  mkdir -p "$MODEL_DIR/VAE"
+
+  pushd "$COMFYUI_DIR" || exit
+  cat >extra_model_paths.yaml <<EOL
 comfyui:
   base_path: ${HOME}/Models
   checkpoints: StableDiffusion
@@ -71,9 +71,11 @@ comfyui:
   upscale_models: Upscale
   vae: VAE
 EOL
-    popd
+  popd
 else # upgrade
+  pushd "$COMFYUI_DIR" || exit
+  source .venv/bin/activate
   uv pip install --upgrade comfy-cli
   comfy update
+  popd
 fi
-
