06b53db163d25239526d2c720a8945126c9f0744
Ben Cochez <ben@kyber.be>
2024-07-24 09:51:16 +0200

    better update pwsh modules

--- scripts/powershell/install.ps1
+++ scripts/powershell/install.ps1
@@ -1,12 +1,14 @@
-$modulesToInstall = @("PSColor", "CompletionPredictor", "PSReadLine", "PSFzf", "PowerType", $(if ($IsWindows) { "PSWindowsUpdate" }))
+$modulesToInstall = @(
+    "PSColor", 
+    "CompletionPredictor",
+    "PSReadLine", 
+    "PSFzf", 
+    "PowerType",
+    $(if ($IsWindows) { "PSWindowsUpdate" }
+    ))
 
 foreach ($module in $modulesToInstall) {
-    if ($null -ne $module) {
-        if (Get-Module -ListAvailable -Name $module) {
-            Update-Module -Name $module
-        }
-        else {
-            Install-Module -Name $module -Scope CurrentUser -Force
-        }
+    if ($null -ne $module -and -not (Get-Module -ListAvailable -Name $module)) {
+        Install-Module -Name $module -Scope CurrentUser -AllowClobber -Force
     }
 }
