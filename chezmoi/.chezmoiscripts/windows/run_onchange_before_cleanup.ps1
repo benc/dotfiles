1a29fb0137272fca4553f92f22a9a1f3a3a7d2d9
Ben Cochez <ben@kyber.be>
2024-11-14 13:51:12 +0100

    windows cleanup script
    decrapifier: remove unneccessary windows stuff

--- chezmoi/.chezmoiscripts/windows/run_onchange_before_cleanup.ps1
+++ chezmoi/.chezmoiscripts/windows/run_onchange_before_cleanup.ps1
@@ -1,69 +1,24 @@
-gsudo {
-    Write-Output "🔧 Cleaning up Windows components we do not need..."
+Write-Output "🔧 Cleaning up unwanted winget packages..."
+$wingetToRemove = @(
+    "IVPN.IVPN",
+    "HaystackSoftwareLLC.Arq7"
+)
 
-    $appsToRemove = @(
-        "Clipchamp.Clipchamp",
-        "Microsoft.BingNews",
-        "Microsoft.BingWeather",
-        "Microsoft.GamingApp",
-        "Microsoft.GetHelp",
-        "Microsoft.Getstarted",
-        "Microsoft.Microsoft3DViewer",
-        "Microsoft.MicrosoftStickyNotes",
-        "Microsoft.MixedReality.Portal",
-        "Microsoft.MSPaint",
-        "Microsoft.OutlookForWindows",
-        "Microsoft.Paint",
-        "Microsoft.People",
-        "Microsoft.Todos_8wekyb3d8bbwe",
-        "Microsoft.ScreenSketch",
-        "Microsoft.SkypeApp",
-        "Microsoft.StorePurchaseApp",
-        "Microsoft.Windows.Photos",
-        "Microsoft.WindowsAlarms",
-        "Microsoft.WindowsCalculator",
-        "Microsoft.WindowsCamera",
-        "Microsoft.WindowsCommunicationsApps",
-        "Microsoft.WindowsFeedbackHub",
-        "Microsoft.WindowsMaps",
-        "Microsoft.WindowsSoundRecorder",
-        "Microsoft.Xbox.TCUI",
-        "Microsoft.XboxApp",
-        "Microsoft.XboxGameOverlay",
-        "Microsoft.XboxGamingOverlay",
-        "Microsoft.XboxIdentityProvider",
-        "Microsoft.XboxSpeechToTextOverlay",
-        "Microsoft.YourPhone",
-        "Microsoft.ZuneMusic",
-        "Microsoft.ZuneVideo"
-    )
-
-    foreach ($app in $appsToRemove) {
-        $userApp = Get-AppxPackage $app -ErrorAction SilentlyContinue
-        if ($userApp) {
-            $userApp | Remove-AppxPackage
-        }
-    
-        $allUsersApp = Get-AppxPackage -AllUsers $app -ErrorAction SilentlyContinue
-        if ($allUsersApp) {
-            $allUsersApp | Remove-AppxPackage
-        }
-    
-        $provisionedApp = Get-AppxProvisionedPackage -Online | Where-Object { $_.DisplayName -eq $app }
-        if ($provisionedApp) {
-            $provisionedApp | ForEach-Object {
-                Remove-AppxProvisionedPackage -Online -AllUsers -PackageName $_.PackageName
-            }
-        }
+foreach ($package in $wingetToRemove) {
+    if (winget list --exact -q $package) {
+        Write-Output "Removing winget package: $package"
+        winget uninstall --exact --silent $package
     }
+}
 
-    Write-Output "🔧 Disabling Windows features we do not want..."
-    $featuresToDisable = @(
-        "MicrosoftWindowsPowerShellv2", # old version of PowerShell
-        "MicrosoftWindowsPowerShellv2Root" # old version of PowerShell
-    )
-    
-    foreach ($feature in $featuresToDisable) {
-        Disable-WindowsOptionalFeature -Online -FeatureName $feature -NoRestart
+Write-Output "🔧 Cleaning up unwanted Scoop packages..."
+$scoopToRemove = @(
+    "extras/tailscale"
+)
+
+foreach ($package in $scoopToRemove) {
+    if (scoop list | Select-String -Pattern "^$package\s") {
+        Write-Output "Removing Scoop package: $package"
+        scoop uninstall $package
     }
 }
