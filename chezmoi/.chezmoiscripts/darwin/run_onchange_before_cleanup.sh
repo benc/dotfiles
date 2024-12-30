5ee673a204e3d9951015f5fa65da1c0b91973454
Ben Cochez <ben@kyber.be>
2024-12-30 22:53:36 +0100

    cleanup cleanup

--- chezmoi/.chezmoiscripts/darwin/run_onchange_before_cleanup.sh
+++ chezmoi/.chezmoiscripts/darwin/run_onchange_before_cleanup.sh
@@ -9,27 +9,3 @@ if ! command -v brew &> /dev/null; then
   echo "brew not found, nothing to clean..."
   exit 0
 fi
-
-# replaced by musescore
-brew remove lilypond || true
-brew remove ly || true
-
-# install separately
-brew remove microsoft-office || true
-
-# superceded by windows app
-brew remove microsoft-remote-desktop || true
-sudo mas uninstall 1295203466 || true # windows app - install it through brew
-sudo mas uninstall 6444602274 || true # ivory
-
-# replaced by tailscale mullvad
-brew remove ivpn || true
-
-# not used
-brew remove postico || true
-brew remove xpipe-io/tap/xpipe || true
-brew untap xpipe-io/tap || true
-brew remove arq || true
-brew remove cursor || true
-brew remove "anythingllm" || true
-brew remove difftastic || true
